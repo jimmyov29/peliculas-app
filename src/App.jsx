f3c480c4306b40b62796befd86054bff6d225d8d
jimmyov29 <jimmy.ov2998@gmail.com>
2023-05-22 14:30:31 -0600

    ladingPages issues

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,17 @@
-import { useEffect } from "react";
-import React from "react";
-// import axios from "axios";
-import "./App.css";
 import { useState } from "react";
-// import YouTube from "react-youtube";
-import {MyRoutes} from "./routers/routes";
-import { LandigPage } from "./pages/LandingPage";
+import reactLogo from "./assets/react.svg";
+import "./App.css";
+import { MyRoutes } from "./routers/routes";
 
 function App() {
-  return (<div>
-        <header>
-      <h1 className="title">Peliculas👍</h1>
-    </header>
-    <MyRoutes/>
-  </div>
+  return (
+    <div>
+      <header>
+        <h1 className="title">🎬Películas</h1>
+      </header>
+      <MyRoutes />
+    </div>
   );
 }
-
 
 export default App;
